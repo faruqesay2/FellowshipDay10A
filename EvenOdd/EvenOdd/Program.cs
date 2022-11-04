9c8dafb30781d08846e578cd14497ea4dcc3d17f
faruqesay2 <faruqesay786@gmail.com>
2022-11-04 10:59:28 +0530

    Day10_Programs

---
+++ EvenOdd/EvenOdd/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating one integer number n to read the user input.
+            int n;
+
+            //Ask the user to enter a number.
+            Console.WriteLine("Enter a number to check : ");
+
+            //Read the number and store it in n.
+            n = int.Parse(Console.ReadLine());
+
+
+            //Using an if-else statement, check if the number is divisible by 2 or not.
+            if (n % 2 == 0)
+            {
+                //If yes, print that the number is an even number.
+                Console.WriteLine(n + " is an even number");
+            }
+            else
+            {
+                //Else, print that the number is an odd number.
+                 Console.WriteLine(n + " is an odd number");
+            }
+        }
+    }
+}
+
+//FARUQESAY
---
+++ ReverseNumber/ReverseNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ReverseNumber
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            int num; //num is the number entered by the user.
+
+            int reverse = 0;  //reverse is the reversed number ie. reverse of num.
+
+
+            Console.WriteLine("Enter the number : ");
+            num = int.Parse(Console.ReadLine());  //It is reading the number and storing that in num.
+
+
+            //The while loop is used to reverse the number.
+            //It keeps adding the rightmost digit of num to reverse.
+            //Once the while loop ends, reverse holds the reversed value of num.
+
+            while (num > 0)
+            {
+                reverse = reverse * 10 + num % 10;
+                num = num / 10;
+            }
+
+            //Finally, it is printing the value of the reversed number i.e. reverse.
+            Console.WriteLine("Reversed number : {0}", reverse);
+
+        }
+    }
+}
+
+//@FARUQESAY
